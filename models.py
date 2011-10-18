ca45c1002fb8dabbd8ec657e629248a1230f07ce
Krishna Kamath <krishna.kamath@gmail.com>
2011-10-17 20:01:36 -0500

    modified models

--- models.py
+++ models.py
@@ -14,22 +14,13 @@ from settings import spamModelFolder
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
+RANDOM_MODEL = 'random'
+NON_SPAM_MODEL = 'non_spam'
+
 class Model(object):
-    def __init__(self, id):
-        self.id = id
-#        self.noOfMessagesPerTimeStep = noOfMessagesPerTimeStep
-#        self.newTopicProbability = newTopicProbability
-        self.analysisFile = spamModelFolder+'model_analysis'
-#    def process(self, currentTimeStep, currentTopics, currentUsers):
-#        for i in range(self.noOfMessagesPerTimeStep):
-#            topic = None
-#            if not currentTopics: Topic.addNewTopics(currentTopics, 300)
-#            if GeneralMethods.trueWith(self.newTopicProbability): 
-#                topic = Topic(len(currentTopics))
-#                currentTopics.append(topic)
-#            else: topic=random.choice(currentTopics)
-#            topic.countDistribution[currentTimeStep]+=1
-#            topic.totalCount+=1
+    def __init__(self, id=RANDOM_MODEL):
+        self.modelFile = spamModelFolder+id
+    def topicSelectionMethod(self, user, currentTopics): return random.choice(currentTopics) 
     def process(self, currentTimeStep, currentTopics, currentUsers, **conf):
         for user in currentUsers:
             if GeneralMethods.trueWith(conf['userMessagingProbability']):
@@ -38,22 +29,24 @@ class Model(object):
                 if GeneralMethods.trueWith(conf['newTopicProbability']): 
                     topic = Topic(len(currentTopics))
                     currentTopics.append(topic)
-                else: topic=random.choice(currentTopics)
+                else: topic=self.topicSelectionMethod(user, currentTopics)
                 topic.countDistribution[currentTimeStep]+=1
                 topic.totalCount+=1
-                
     def analysis(self, currentTimeStep=None, currentTopics=None, currentUsers=None, modeling=True):
         if modeling:
             topicDistribution = dict((str(topic.id), {'total': topic.totalCount, 'timeStep': topic.countDistribution[currentTimeStep]}) for topic in currentTopics)
             print currentTimeStep
-            FileIO.writeToFileAsJson({'t':currentTimeStep, 'topics':topicDistribution}, self.analysisFile)
+            FileIO.writeToFileAsJson({'t':currentTimeStep, 'topics':topicDistribution}, self.modelFile)
         else:
             topicsDataX = defaultdict(list)
             topicsDataY = defaultdict(list)
-            for data in FileIO.iterateJsonFromFile(self.analysisFile):
+            for data in FileIO.iterateJsonFromFile(self.modelFile):
                 for topic in data['topics']: topicsDataX[topic].append(data['t']), topicsDataY[topic].append(data['topics'][topic]['timeStep'])
             for topic in topicsDataX: plt.fill_between(topicsDataX[topic], topicsDataY[topic], color=GeneralMethods.getRandomColor(), alpha=0.6)
             plt.show()
+            
+class NonSpamModel(Model):
+    pass
     
         
 def run(model, numberOfTimeSteps, analysisMethod, 
@@ -71,8 +64,8 @@ def run(model, numberOfTimeSteps, analysisMethod,
         analysis.call(currentTimeStep, currentTimeStep=currentTimeStep, currentTopics=currentTopics, currentUsers=currentUsers)
 
 if __name__ == '__main__':
-    model=Model(1)
-    GeneralMethods.runCommand('rm -rf %s'%model.analysisFile)
+    model=Model()
+    GeneralMethods.runCommand('rm -rf %s'%model.modelFile)
     conf = { 'model': model, 'numberOfTimeSteps': 200, 'analysisMethod': model.analysis,
             'newTopicProbability': 0.001, 'userMessagingProbability': 0.1,
             }
